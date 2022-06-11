423768dba101d73e29c243eb5e7ad4045cc72780
Ludvig Lindholm <ludviglindholm127@gmail.com>
2022-06-11 16:28:24 +0200

    Fixed some weird bug.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
-#include <iostream>;
-#include <ctime>;
-#include <cstdlib>;
-
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
-
 
 int random() {
     return rand() % 36 +1;
 }
 
 void println(string to_print) {
-
     std::cout << to_print << std::endl;
 }
 
+bool binary_choice(string question, string answ1, string answ2) {
+    // Returns true if the first answ is chosen else false.
+    bool ok_answ = false;
+    string answ;
+    while (!ok_answ) {
+        println(question);
+        cin >> answ;
+        if (answ == answ1 || answ == answ2) {
+            ok_answ = true;
+        }
+    }
+    return answ == answ1 ? true : false;
+}
 
 int main() {
     srand (time(0));
@@ -22,51 +32,34 @@ int main() {
     bool allowed_selection;
     int balance = 1000;
     string f_or_n;
-
     while (play) {
-        if (allowed_selection) {
-            println("Vill du spela igen (y/n): ");
-            string play_again;
-            cin >> play_again;
-            if (play_again == "n") {
-                break;
-            }
+        if (balance < 100) {
+            println("Du kan inte spela mer du har bara" + std::to_string(balance));
+            println("Du har förlorat...");
+            break;
         }
-
         allowed_selection = false;
         while (!allowed_selection) {
             println("");
             println("Du har " + std::to_string(balance));
             println("Välj en insats, 100, 300 eller 500 (kr): ");
             cin >> bet;
-            balance -= bet;
-            allowed_selection = (bet == 100 || bet == 300 || bet == 500);
+            allowed_selection = (bet == 100 || bet == 300 || bet == 500) && bet <= balance;
         }
+        balance -= bet;
 
-        allowed_selection = false;
-
-        while (!allowed_selection) {
-            println("Vill du satsa på en färg eller på en siffra? (f/n)");
-            cin >> f_or_n;
-            allowed_selection = (f_or_n == "f" ||f_or_n == "n");
-        }
-
-        allowed_selection = false;
-        if (f_or_n == "f") {
-            string color;
-            while (!allowed_selection) {
-                println("Vilken färg vill du satsa på? (rod/svart): ");
-                cin >> color;
-                allowed_selection = (color == "rod" ||color == "svart");
-            }
-            if (random()%2==0 && color == "svart" || random()%2==1 && color == "rod") {
+        bool color_bet = binary_choice(("Vill du satsa på en färg eller på en siffra? (f/n)"), "f", "n");
+        if (color_bet) {
+            bool color = binary_choice("Vilken färg vill du satsa på? (rod/svart): ", "rod", "svart");
+            if (random()%2==0 && !color || random()%2==1 && color) {
                 println("Vann med rätt färg");
                 balance += bet*3;
             } else {
-                println("Gissade på fel färg :C");
+                println(("Gissade på fel färg, korrekt färg var " + std::string(!color ? "röd" : "svart")));
             }
         } else {
             string number;
+            allowed_selection = false;
             while (!allowed_selection) {
                 println("Vilken siffra gissar du på?: ");
                 cin >> number;
@@ -79,7 +72,7 @@ int main() {
                 println("Gissade på fel siffra :C");
             }
         }
+        if (!binary_choice("Vill du spela igen (y/n): ", "y", "n")) break;
     }
-
     return 0;
 }
